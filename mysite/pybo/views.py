4d36b322e62d150dc282434a4ce6a4b934895f02
JiWon <radiohead@knou.ac.kr>
2022-05-13 14:40:41 +0900

    5월 13일
    질문 리스트 들어가기
    답변 등록 구현
    세부페이지 id 뒤에 //이렇게 되는거
    임시로 해결은 했는데 왜그런지 모르겠음

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. \n장고에 오신것을 환영합니다.")
+    """ pybo 목록 출력 """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """ pybo 내용 출력"""
+    #question = Question.objects.get(id=question_id) 예외처리 없는거 (없는 페이지 방문 시 디버그페이지)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """pybo 답변 등록"""
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
+
