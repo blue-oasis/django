542f56d58ac8a21634e7f1c14723d7b285022a50
JiWon <radiohead@knou.ac.kr>
2022-06-27 16:05:09 +0900

    마크다운 적용
    게시판에 마크다운 뭄법 사용해 글 작성할 수 있도록 추가
     mark 필터 추가
    question_detail에 적용

--- mysite/pybo/templatetags/pybo_filter.py
+++ mysite/pybo/templatetags/pybo_filter.py
@@ -1,7 +1,15 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+# markdown 문법을 HTML으로 변환하는 필터 함수
+@register.filter()
+def mark(value):
+    extensions = ["nl2br", "fenced_code"] #nl2br 줄바꿈 문자 <br> 변환, fenced_code 마크다운 소스코드 표현
+    return mark_safe(markdown.markdown(value, extensions=extensions))
